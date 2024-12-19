cf3c58f4da629eee44612cb6a4f9eacec885e319
lsalug00 <lidiasaludes@gmail.com>
2024-12-19 16:04:58 +0100

    carrusel automatico

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
+const tiempo = 3000
+let intervalo = setInterval(autoSlide, tiempo);
+
+function autoSlide() {
+  plusSlides(1);
+}
+
+function resetAutoSlide() {
+  clearInterval(intervalo);
+  intervalo = setInterval(autoSlide, tiempo);
+}
+
 function plusSlides(n) {
+  resetAutoSlide();
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
+  resetAutoSlide();
   showSlides(slideIndex = n);
 }
 
@@ -15,14 +27,14 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let botones = document.getElementsByClassName("boton");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1; }
+  if (n < 1) { slideIndex = slides.length; }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < botones.length; i++) {
     botones[i].className = botones[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  botones[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  botones[slideIndex - 1].className += " active";
 }
